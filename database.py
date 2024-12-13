2ce90dbe42a6e6a0de67d6a7ff85cb695600403e
Elizabeth Orellana <eorellana96@csu.fullerton.edu>
2024-12-12 21:26:35 -0800

    initialized/set up database on atlas. Subscription Plan Management API seems to be working

--- database.py
+++ database.py
@@ -1,12 +1,16 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
 # mongoDB connection string
-MONGO_URI = 'mongodb://127.0.0.1:27017'
-DATABASE_NAME = 'cloud_service_access'
+MONGO_URI = 'mongodb+srv://eorellana02:CPSC449@cloudservice.3je2l.mongodb.net/cloudservice?retryWrites=true&w=majority&appName=cloudservice'
+DATABASE_NAME = 'cloudservice'
 
 # create mongoDB client and database
 client = AsyncIOMotorClient(MONGO_URI)
 db = client[DATABASE_NAME]
 
 # collections
-plans_collection = db['plans']
+sub_plans_collection = db['plans']
+permissions_collection = db['permissions']
+user_subs_collection = db['user_subs']
+access_collection = db['access']
+usage_collection = db['usage']
